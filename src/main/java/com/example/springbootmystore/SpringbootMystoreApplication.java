ae14dac037b31bc472a46b07740425d50bcdd728
Fredy Jimenez <fredy.alberto.jimenez@pivotree.com>
2021-06-09 12:04:37 -0500

    Initial commit

---
+++ src/main/java/com/example/springbootmystore/SpringbootMystoreApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootmystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class SpringbootMystoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMystoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootmystore/SpringbootMystoreApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.springbootmystore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class SpringbootMystoreApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
